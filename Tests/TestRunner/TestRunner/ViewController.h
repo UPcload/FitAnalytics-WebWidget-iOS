2ea22cb9c7b0896d20dcde55572aff597bb2a808
Tomas Jelen <tomas@delikates.org>
2017-01-31 12:04:02 +0100

    test runner prototype for controlling the widget

---
+++ Tests/TestRunner/TestRunner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestRunner
+//
+//  Created by Tomas Jelen on 30/01/17.
+//  Copyright © 2017 FitAnalytics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
